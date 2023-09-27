790ad6c9d75e587b1b58197ee13b3780d8779da3
ramon <ramon_rib@hotmail.com>
2023-09-27 02:33:39 -0300

    fix, container output

--- worflows/workflow-dasf-seismic.py
+++ worflows/workflow-dasf-seismic.py
@@ -16,7 +16,6 @@ with Workflow(
     generate_name="dag-target-",
     entrypoint="dag-target",
     namespace="argo",
-    arguments=Parameter(name="target", value="E"),
 ) as w:
     dasf_scheduler = Container(
         name="dasf-scheduler",
@@ -95,3 +94,45 @@ with Workflow(
         scheduler_deployment >> worker_deployment >> reference_deployment
 
 w.create()
+
+
+# from hera.expr import g
+# from hera.workflows import (
+#     DAG,
+#     ContainerNode,
+#     ContainerSet,
+#     Parameter,
+#     Script,
+#     Task,
+#     Workflow,
+#     models as m,
+# )
+
+
+# def _check():
+#     assert "{{inputs.parameters.x}}" == "hi"
+
+
+# with Workflow(
+#     generate_name="outputs-result-",
+#     entrypoint="main",
+# ) as w:
+#     with ContainerSet(name="group") as group:
+#         ContainerNode(name="main", image="python:alpine3.6", command=["python", "-c"], args=['print("hi")\n'])
+
+#     verify = Script(
+#         source=_check,
+#         image="python:alpine3.6",
+#         command=["python"],
+#         inputs=[Parameter(name="x")],
+#         add_cwd_to_sys_path=False,
+#         name="verify",
+#     )
+#     with DAG(name="main") as dag:
+#         a = Task(name="a", template=group)
+#         b = Task(
+#             name="b",
+#             arguments=m.Arguments(parameters=[m.Parameter(name="x", value=f"{g.tasks.a.outputs.result:$}")]),
+#             template=verify,
+#             dependencies=["a"],
+#         )
